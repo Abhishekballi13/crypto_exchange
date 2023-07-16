1d9113e5cb5b3267c3de603900b584a55ddb39c7
unknown <ballig360@gmail.com>
2023-07-16 10:08:28 +0530

    some minor changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+const avatarSrc = "https://media.licdn.com/dms/image/D4D03AQFfj-KZmQmrCw/profile-displayphoto-shrink_800_800/0/1679216384639?e=1695254400&v=beta&t=XvOjryGW4qljKPQGimDcrlPsKIgc7XU8FwwTjXJUyns";
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            We are the best crypto trading app in India, we provide our guidance
+            at a very reasonable price.
+          </Text>
+        </VStack>
+
+        <VStack>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+          <Text>Our Founder</Text>
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
